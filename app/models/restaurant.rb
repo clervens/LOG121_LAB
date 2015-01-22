8d71e53d87bfb3bff95c2488f44e6fb582213457
Clervens Volcy <clervens.volcy@gmail.com>
2015-01-22 16:34:05 -0500

    MOD : update annotate config

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id                :integer          not null, primary key
+#  nom               :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#  mock_restaurateur :string(255)
+#
+
 class Restaurant < ActiveRecord::Base
 end
