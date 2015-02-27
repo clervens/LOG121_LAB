a2e051a11b8af255e24543e2bb93a6f9f1d26569
Clervens Volcy <clervens.volcy@gmail.com>
2015-02-27 14:39:36 -0500

    ADD : test for restaurants

--- spec/factories/restaurants_factories.rb
+++ spec/factories/restaurants_factories.rb
@@ -1,5 +1,13 @@
 FactoryGirl.define do
   factory :restaurant do
     nom {random_string} 
+    factory :restaurant_with_menus do
+      transient do
+        menus_count 5
+      end
+      after(:create) do |restaurant, evaluator|
+        create_list(:menu_with_plats, evaluator.menus_count, restaurant: restaurant)
+      end
+    end
   end
 end
--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'can be instanciated' do
+    before do
+      @restaurant = FactoryGirl.create(:restaurant)
+    end
+
+    it 'and have menus' do
+      @restaurant = FactoryGirl.create(:restaurant_with_menus, :menus_count => 10)
+      expect(@restaurant.menus.count).to eq 10
+    end
+  end
+  describe 'should be able to interact with mock_restaurateur' do
+    it 'by responding to it' do
+      restaurant = FactoryGirl.create(:restaurant)
+      expect(restaurant).to respond_to(:mock_restaurateur)
+    end
+    it 'by setting it' do
+      restaurateur_name = 'alfred'
+      restaurant = FactoryGirl.create(:restaurant, mock_restaurateur: restaurateur_name)
+      expect(restaurant.mock_restaurateur).to eq(restaurateur_name)
+    end
+    it 'through the alias restaurateur' do
+      restaurateur_name = 'alfred'
+      restaurant = FactoryGirl.create(:restaurant, mock_restaurateur: restaurateur_name)
+      expect(restaurant.mock_restaurateur).to eq(restaurant.restaurateur)
+    end
+  end
 end
