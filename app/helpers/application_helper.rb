87331bd3af2838b8cd882d7df7e1e144aae38994
Clervens Volcy <clervens.volcy@gmail.com>
2015-03-19 10:44:10 -0400

    MOD : update layout in footer and display rendering time in footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
   rescue ActionView::MissingTemplate => e
     nil
   end
+
+  def render_time
+    sprintf('%.3f', (Time.now.usec - @start_time).abs / 1000000.0)
+  end
 end
