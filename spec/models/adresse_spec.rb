fb0a72ff91474b5285a4991ef26096bc8523af57
Clervens Volcy <clervens.volcy@gmail.com>
2015-03-31 10:37:41 -0400

    ADD : adresse specs

--- spec/models/adresse_spec.rb
+++ spec/models/adresse_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Adresse, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'can be instanciated' do
+    before do
+      @adresse = Adresse.new
+    end
+
+    it 'and respond_to value' do
+      @adresse.should respond_to(:valus)
+    end
+  end
 end
