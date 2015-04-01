794a7f7387fb029db81122fbee687ab91c7361b8
Clervens Volcy <clervens.volcy@gmail.com>
2015-04-01 18:54:10 -0400

    MOD : update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,21 @@ User.create!({
   confirmed_at: DateTime.now
 })
 user = User.find_by(email: 'admin@example.com')
-  user.add_role :entrepreneur
-  user.add_role :restaurateur
-  user.add_role :livreur
-  user.add_role :client
+
+[:entrepreneur, :restaurateur, :livreur, :client].each do |role|
+  n = User.new({
+    nom: role.capitalize,
+    prenom: 'LOGTI',
+    telephone: ENV["Default_SMS_Number"],
+    email: "#{role.to_s}@example.com",
+    password: 'password',
+    password_confirmation: 'password',
+    confirmed_at: DateTime.now
+  })
+  n.add_role role
+  n.save!
+  user.add_role role
+end
 
 ## Restaurants
 
@@ -29,40 +40,73 @@ Restaurant.create!([{
   nom: "Noma",
   adresse: "200 Rue Beaubien Est, Montréal, QC H2S 1R4"
 }, {
-  nom: "Eleven Madison Park",
-  adresse: "New York, USA"
+  nom: "Europea",
+  adresse: "1227 Rue de la Montagne, Montréal, QC H3G 1Z2"
 }, {
   nom: "Bouillon Bilk",
   adresse: "1595 Boul Saint-Laurent, Montréal, Québec H2X 2S9, Canada"
+}, {
+  nom: "Evy ting Wong![TEST]",
+  adresse: "8335 Boul Langelier, Montréal, QC H1P 2B8"
 }])
 
 ## Menus et Plats
 
 Menu.create!([{
   nom: "Spéciaux",
-  restaurant_id: 3,
+  restaurant_id: 1,
   plats_attributes: [{
       nom: "McDouble Xtrem Plus",
       description: "S.O.",
-      prix: "6.0",
+      prix: "6.0"
     }, {
       nom: "Bob Burger",
       description: "S.O.",
-      prix: "5.0",
+      prix: "5.0"
     }],
   }, {
-  nom: "Le menu festin",
+  nom: "Soir",
+  restaurant_id: 3,
+  plats_attributes: [{
+      nom: "Hamachi",
+      description: "S.O.",
+      prix: "16.25"
+    }, {
+      nom: "Assortiment de fromages québécois",
+      description: "S.O.",
+      prix: "20.0"
+    }],
+  }, {
+  nom: "Menu dégustation signature",
   restaurant_id: 2,
   plats_attributes: [{
-      nom: "McDouble Xtrem Plus",
-      description: "S.O.",
-      prix: "3.75",
+      nom: "Risotto",
+      description: "Risotto crémeux mascarpone et burrata. Poêlée de champignons et asperges de saison. Mousseline de petits pois et vrilles.
+Oeuf en coquille de béarnaise et crumble",
+      prix: "23.50"
     }, {
-      nom: "Sushi",
-      description: "S.O.",
-      prix: "8.25",
+      nom: "Calamar",
+      description: "Calamar citronné et structuré en tagliatelles. OEuf de caille poché, croutons d’encre de seiche au beurre à l’ail",
+      prix: "23.50"
+    }, {
+      nom: "Cerf",
+      description: "Tartare de cerf boucané sous cloche. Pommes allumettes et croutons au wasabi. Réduction de carminé du Québec
+et vieux cheddar. Émulsion de moutarde",
+      prix: "25.50"
+    }, {
+      nom: "Fruits de Mer",
+      description: "Calamar citronné et structuré en tagliatelles. OEuf de caille poché, croutons d’encre de seiche au beurre à l’ail",
+      prix: "26.50"
     }],
 }])
+
+menu = { nom: "BIG @$$ Menu", restaurant_id: 4, plats_attributes: [] }
+5000.times do |index|
+  nom = ('a'..'z').to_a.sample(10).join
+  description = ('a'..'z').to_a.sample(30).join
+  prix = (rand(100)+rand).round 2
+  menu[:plats_attributes] << { nom: nom, description: description, prix: prix }
+end
 
 ## Adresses
 
@@ -70,31 +114,37 @@ Menu.create!([{
   "1100 rue Notre-Dame Ouest, Montréal, QC H3C 1K3",
   "1111 rue Notre-Dame Ouest, Montréal, QC H3C 6M8",
   "1909 Avenue des Canadiens-de-Montréal, Montréal, QC H4B 5G0",
-  "845 Rue Sherbrooke O, Montréal, QC H3A 0G4",
-  "4777 Avenue Pierre-de Coubertin, Montréal, QC H1V 1B3"
+  "845 Rue Sherbrooke O, Montréal, QC H3A 0G4"
 ].each do |address|
   Adresse.create!(value: address, user_id: 1)
 end
 
+User.all.each do |user|
+  Adresse.create! value: "4777 Avenue Pierre-de Coubertin, Montréal, QC H1V 1B3", user_id: user
+end
+
 ## Commandes
+ENV['SKIP_SMS'] = 'true'
 
 Commande.create!([{
     date_de_livraison: 1.hour.from_now,
     restaurant_id: 1,
     adresse_id: 1,
     ligne_commandes_attributes: [{qte: 70, plat_id: 1}],
-    user_id: 1
+    user_id: 1,
+    etat: Commande.etats[:paye]
   }, {
     date_de_livraison: 3.hour.from_now,
     restaurant_id: 2,
     adresse_id: 1,
-    ligne_commandes_attributes: [{qte: 2, plat_id: 1}, {qte: 1, plat_id: 2}],
-    user_id: 1
+    ligne_commandes_attributes: [{qte: 2, plat_id: 5}, {qte: 1, plat_id: 6}],
+    user_id: 1,
+    etat: Commande.etats[:paye]
   }, {
     date_de_livraison: 4.days.from_now,
     restaurant_id: 3,
     adresse_id: 1,
-    ligne_commandes_attributes: [{qte: 3, plat_id: 1}, {qte: 5, plat_id: 2}, {qte: 8, plat_id: 3}, {qte: 14, plat_id: 4}],
+    ligne_commandes_attributes: [{qte: 3, plat_id: 3}, {qte: 5, plat_id: 4}],
     user_id: 1,
     etat: Commande.etats[:prete]
 }])
